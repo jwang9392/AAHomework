707ec6ec87dc686cbb7cbc6d9103607f1110d8b8
jwang9392 <jwang9392@gmail.com>
2019-12-05 22:52:24 -0500

    W3D5 added test cases to diy_adts.rb

--- W3D5/diy_adts.rb
+++ W3D5/diy_adts.rb
@@ -63,7 +63,7 @@ class Map
     def delete(key)
         @map.each_with_index do |kv_pair, idx|
             if kv_pair[0] == key
-                return @map.take(idx) + @map.drop(idx + 1)
+                @map.delete_at(idx)
             end
         end
     end 
@@ -74,5 +74,37 @@ class Map
 end
 
 if __FILE__ == $PROGRAM_NAME
+    # #exercise1 - stack tests
+    # new_stack = Stack.new
+    # new_stack.push(1)
+    # new_stack.push(2)
+    # new_stack.push(7)
+    # p new_stack
+    # new_stack.pop
+    # p new_stack.stack
+    # p new_stack.peek
+    # p new_stack.stack
+
+    # #exercise2 - queue tests
+    # new_q = Queue.new
+    # new_q.enqueue("hi")
+    # new_q.enqueue("there")
+    # new_q.enqueue("friend")
+    # p new_q
+    # new_q.dequeue
+    # p new_q.queue
+    # p new_q.peek
+    # p new_q.queue
+
+    # #exercise3 - map test
+    # new_map = Map.new
+    # new_map.set(1, "hello")
+    # p new_map.show
+    # new_map.set(1, "bye")
+    # p new_map.show
+    # p new_map.get(1)
+    # new_map.set(2, "I SAID GOOD DAY")
+    # new_map.delete(1)
+    # p new_map.show
 
 end
